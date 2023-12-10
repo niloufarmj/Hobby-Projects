b4283baeb8cfbf2f438741dfea64c589f5ad2fc8
Niloufar Moradi Jam <46811379+niloufarmj@users.noreply.github.com>
2023-12-10 22:13:42 +0330

    Add Collision and Collect Objects - Roll a Ball

--- rollaball/Rollaball/Assets/Scripts/PlayerController.cs
+++ rollaball/Rollaball/Assets/Scripts/PlayerController.cs
@@ -33,4 +33,12 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
 }
